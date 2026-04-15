c6028de2f10106215d2b4482215c679239671e55
KeroHocTapOnline <nhockzukon9x@gmail.com>
2026-04-15 16:26:04 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  TOKEN: "THAY_TOKEN_CUA_BAN_VAO_DAY",
+  ALLOWED_GUILDS: ["1295359112053391423"], // ID server của bạn
+  OWNER_ID: "453518925997670411", // ID owner (dùng cho một số lệnh admin)
+  ANNOUNCE_CHANNEL_ID: "1389473240824414306", // kênh thông báo playtime role (nếu cần)
+  VN_TZ: "Asia/Ho_Chi_Minh",
+  DEFAULT_POINT_PER_MSG: 2,
+  DEFAULT_COOLDOWN_SEC: 10,
+  CAT_PRICE: 1500
+};
