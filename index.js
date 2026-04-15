417e9e57e5abb2b8cad953ca024d5eb7d7a9fc10
KeroHocTapOnline <nhockzukon9x@gmail.com>
2026-04-15 16:25:08 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const config = require('./config');
+const db = require('./database');
+const cron = require('./utils/cron');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`✅ Bot ${client.user.tag} đã online!`);
+  require('./events/ready')(client);
+  cron.startPetNotifications(client);
+});
+
+client.on('messageCreate', (message) => require('./events/messageCreate')(message, client));
+client.on('voiceStateUpdate', (oldState, newState) => require('./events/voiceStateUpdate')(oldState, newState, client));
+
+client.login(config.TOKEN);
