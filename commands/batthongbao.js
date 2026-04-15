778b743606941fc41ae97c0e6959bb6e491def87
KeroHocTapOnline <nhockzukon9x@gmail.com>
2026-04-15 17:28:02 +0700

    Update batthongbao.js

--- commands/batthongbao.js
+++ commands/batthongbao.js
@@ -1,15 +1,14 @@
-
-
-// commands/tatthongbao.js
+// commands/batthongbao.js
 const { SlashCommandBuilder } = require('discord.js');
 const db = require('../database');
 const config = require('../config');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('tatthongbao').setDescription('Tắt thông báo mèo'),
+    data: new SlashCommandBuilder().setName('batthongbao').setDescription('Bật thông báo mèo'),
     async execute(interaction) {
         if (!config.ALLOWED_GUILDS.includes(interaction.guild.id)) return interaction.reply({ content: '❌ Server không được phép!', ephemeral: true });
-        db.prepare('INSERT OR REPLACE INTO notifications (user_id, enabled) VALUES (?, 0)').run(interaction.user.id);
-        await interaction.reply('✅ Đã **tắt** thông báo mèo!');
+        db.prepare('INSERT OR REPLACE INTO notifications (user_id, enabled) VALUES (?, 1)').run(interaction.user.id);
+        await interaction.reply('✅ Đã **bật** thông báo mèo lúc 18:00 hàng ngày!');
     }
 };
+
